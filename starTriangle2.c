9650c26dd875fc416238f3371900f69ab1df767f
Subham <164215341+LearnAISubham7@users.noreply.github.com>
2024-04-18 22:46:22 +0530

    Add files via upload

---
+++ starTriangle2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+    int n, m;
+    printf("Enter number of rows : ");
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++)
+    {
+        int a = n - i;
+        for (int j = 1; j <= a; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}
